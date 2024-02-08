661f773f7947c02b248d35765d859bf49bcad26e
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-02-08 12:19:19 +0530

    Update Logs.py

--- PubliceMusicBot/plugins/exambot/Logs.py
+++ PubliceMusicBot/plugins/exambot/Logs.py
@@ -1,36 +1,51 @@
 import random
-from pyrogram import Client
 from pyrogram.types import Message
-from pyrogram import filters
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.types import(InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto, InputMediaVideo, Message)
 from config import PUBLICELOGS
 from PubliceMusicBot import app  
 from PubliceMusicBot.utils.database import get_served_chats
 
-async def lul_message(chat_id: int, message: str):
-    await app.send_message(chat_id=chat_id, text=message)
+async def new_message(chat_id: int, message: str, reply_markup=None):
+    await app.send_message(chat_id=chat_id, text=message, reply_markup=reply_markup)
 
 @app.on_message(filters.new_chat_members)
 async def on_new_chat_members(client: Client, message: Message):
     if (await client.get_me()).id in [user.id for user in message.new_chat_members]:
         added_by = message.from_user.mention if message.from_user else "ᴜɴᴋɴᴏᴡɴ ᴜsᴇʀ"
-        matlabi_jhanto = message.chat.title
-        served_chats = len(await get_served_chats())
+        title = message.chat.title
+        username = f"@{message.chat.username}"
         chat_id = message.chat.id
-        if message.chat.username:
-            chatusername = f"@{message.chat.username}"
-        else:
-            chatusername = "ᴩʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ"
-        lemda_text = f"✫ #NEW_GROUP ✫\n\n┏━━━━━━━━━━━━━━━━━┓\n✫ 𝐂ʜᴀᴛ 𝐓ɪᴛʟᴇ : {matlabi_jhanto}\n✫ 𝐂ʜᴀᴛ 𝐈ᴅ : {chat_id}\n✫ ᴄʜᴀᴛ ᴜɴᴀᴍᴇ : {chatusername}\n✫ ᴀᴅᴅᴇᴅ ʙʏ : {added_by}"
-        await lul_message(PUBLICELOGS, lemda_text)
+        am = f"✫ <b><u>ɴᴇᴡ ɢʀᴏᴜᴘ</u></b> :\n\nᴄʜᴀᴛ ɪᴅ : {chat_id}\nᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ : {username}\nᴄʜᴀᴛ ᴛɪᴛʟᴇ : {title}\n\nᴀᴅᴅᴇᴅ ʙʏ : {added_by}"
+        reply_markup = InlineKeyboardMarkup([
+    [
+        InlineKeyboardButton(
+            message.from_user.first_name,
+            user_id=message.from_user.id
+        )
+    ]
+])
+
         
+        await new_message(PUBLICELOGS, am, reply_markup)
 
 @app.on_message(filters.left_chat_member)
-async def on_left_chat_member(_, message: Message):
-    if (await app.get_me()).id == message.left_chat_member.id:
-        remove_by = message.from_user.mention if message.from_user else "𝐔ɴᴋɴᴏᴡɴ 𝐔sᴇʀ"
+async def on_left_chat_member(client: Client, message: Message):
+    if (await client.get_me()).id == message.left_chat_member.id:
+        remove_by = message.from_user.mention if message.from_user else "ᴜɴᴋɴᴏᴡɴ ᴜsᴇʀ"
         title = message.chat.title
-        username = f"@{message.chat.username}" if message.chat.username else "𝐏ʀɪᴠᴀᴛᴇ 𝐂ʜᴀᴛ"
+        username = f"@{message.chat.username}"
         chat_id = message.chat.id
-        left = f"✫ #𝐋ᴇғᴛ_𝐆ʀᴏᴜᴘ ✫\n✫ 𝐂ʜᴀᴛ 𝐓ɪᴛʟᴇ : {title}\n✫ 𝐂ʜᴀᴛ 𝐈ᴅ : {chat_id}\n✫ 𝐑ᴇᴍᴏᴠᴇᴅ 𝐁ʏ : {remove_by}\n✫ 𝐁ᴏᴛ : @{app.username}"
-        await app.send_message(PUBLICELOGS, left)
+        ambye = f"✫ <b><u>ʟᴇғᴛ ɢʀᴏᴜᴘ</u></b> :\n\nᴄʜᴀᴛ ɪᴅ : {chat_id}\nᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ : {username}\nᴄʜᴀᴛ ᴛɪᴛʟᴇ : {title}\n\nʀᴇᴍᴏᴠᴇᴅ ʙʏ : {remove_by}"
+        reply_markup = InlineKeyboardMarkup([
+    [
+        InlineKeyboardButton(
+            message.from_user.first_name,
+            user_id=message.from_user.id
+        )
+    ]
+])
+
+        
+        await new_message(PUBLICELOGS, ambye, reply_markup)
