8de3741ecb2ca78257325d814b59bf1b1867030b
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-02-10 00:40:17 +0530

    Update vars.py

--- PubliceMusicBot/plugins/sudo/vars.py
+++ PubliceMusicBot/plugins/sudo/vars.py
@@ -7,12 +7,12 @@ from PubliceMusicBot import app
 from PubliceMusicBot.misc import SUDOERS
 from PubliceMusicBot.utils.formatters import convert_bytes
 
-@app.on_message(filters.command(["v","var","config"]) & SUDOERS)
+@app.on_message(filters.command(["v","config"]) & SUDOERS)
 async def varsFunc(client, message):
     mystic = await message.reply_text(
         "Please wait.. Getting your config"
     )
-    up_r = f"[Repo]({config.UPSTREAM_REPO})"
+    up_r = f"<a href={config.UPSTREAM_REPO}>Repo</a>"
     up_b = config.UPSTREAM_BRANCH
     owners = config.OWNER_ID
     tg_aud = convert_bytes(config.TG_AUDIO_FILESIZE_LIMIT)
@@ -55,21 +55,21 @@ async def varsFunc(client, message):
 
     text = f"""MUSIC BOT CONFIG :
 UPSTREAM_REPO : {up_r}
-UPSTREAM_BRANCH = {up_b}
-API ID : {api}
-API HASH : {hash}
-BOT TOKEN : {bot}
-MONGO DB : {mongo}
-Assis ID : {sesson}
+UPSTREAM_BRANCH = <code>{up_b}</code>
+API ID : <code>{api}</code>
+API HASH : <code>{hash}</code>
+BOT TOKEN : <code>{bot}</code>
+MONGO DB : <code>{mongo}</code>
+Assis ID : <code>{sesson}</code>
 OWNER USERNAME : @{owner}
-LOG Group Id : {log}
-GIT_TOKEN : {token}
-AUTO_LEAVING_ASSISTANT : {ass}
-ASSISTANT_LEAVE_TIME : {auto_leave} seconds
-SPOTIFY_CLIENT_ID : {sotify}
-SPOTIFY_CLIENT_SECRET : {sotify}
-TG_AUDIO_FILESIZE_LIMIT : {tg_aud}
-TG_VIDEO_FILESIZE_LIMIT : {tg_vid}
+LOG Group Id : <code>{log}</code>
+AUTO_LEAVING_ASSISTANT : <code>{ass}</code>
+ASSISTANT_LEAVE_TIME : <code>{auto_leave}</code> seconds
+SPOTIFY_CLIENT_ID : <code>{sotify}</code>
+SPOTIFY_CLIENT_SECRET : <code>{sotify}</code>
+TG_AUDIO_FILESIZE_LIMIT : <code>{tg_aud}</code>
+TG_VIDEO_FILESIZE_LIMIT : <code>{tg_vid}</code>
+GIT TOKEN : <code>{token}</code>
     """
     await asyncio.sleep(1)
     await mystic.edit_text(text)
