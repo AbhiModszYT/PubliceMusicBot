1020df464f976c2a0504a1cf5654a4198900beca
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-02-09 22:42:23 +0530

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -3,9 +3,12 @@ from typing import List
 
 import yaml
 
+commands = {}
 languages = {}
 languages_present = {}
 
+def get_command(value: str) -> List:
+    return commands["command"][value]
 
 def get_string(lang: str):
     return languages[lang]
